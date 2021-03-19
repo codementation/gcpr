c0d286b02c02779ddce436d1eac8f9a7e9d4a550
Codementation, LLC <support@codementation.com>
2021-03-19 13:16:52 -0500

    adds Tailwind/NetlifyCMS workaround

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,6 +2,6 @@ import CMS from 'netlify-cms-app'
 import HomePagePreview from './preview-templates/HomePagePreview'
 import ForensicsPreview from './preview-templates/ForensicsPreview'
 
-CMS.registerPreviewStyle("https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css");
+CMS.registerPreviewStyle("/admin/main.css");
 CMS.registerPreviewTemplate('index', HomePagePreview)
 CMS.registerPreviewTemplate('forensics', ForensicsPreview)
