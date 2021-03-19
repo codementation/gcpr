9c1bb0a13ebb364d65884f85ee6526ccf344b005
Codementation, LLC <support@codementation.com>
2021-03-19 13:39:02 -0500

    fixes cms admin css order and path

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,6 +2,6 @@ import CMS from 'netlify-cms-app'
 import HomePagePreview from './preview-templates/HomePagePreview'
 import ForensicsPreview from './preview-templates/ForensicsPreview'
 
-CMS.registerPreviewStyle("public/admin/main.css");
+CMS.registerPreviewStyle("/admin/main.css");
 CMS.registerPreviewTemplate('index', HomePagePreview)
 CMS.registerPreviewTemplate('forensics', ForensicsPreview)
